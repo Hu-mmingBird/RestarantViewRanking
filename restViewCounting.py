b61c73dbca7a2d2977e8888ee9eaba20698d4986
yhLee11 <mrncd1@gmail.com>
2021-10-06 18:26:15 +0900

    keyword, company name

--- restViewCounting.py
+++ restViewCounting.py
@@ -3,7 +3,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 import os
 import sys
-
+import numpy as np
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 JSON_PATH=THIS_FOLDER+'/automationproj-327806-6e9bf5c39245.json'
 scope = ['https://spreadsheets.google.com/feeds',
@@ -17,5 +17,9 @@ doc=gc.open_by_url(spreadsheet_url)
 
 worksheet=doc.worksheet('2019년 월보장')
 print(worksheet)
-row_data=worksheet.row_values(1)
-print(row_data)
+
+keyword=worksheet.col_values(3)
+company=worksheet.col_values(5)
+key_com=dict(zip(keyword,company))
+print(key_com)
+    
